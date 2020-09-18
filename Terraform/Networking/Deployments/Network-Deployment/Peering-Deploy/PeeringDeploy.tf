1c12d7d433ed5845dde66fd4059d0ccb30944657
Adin Ermie <Adin.Ermie@outlook.com>
2020-09-18 13:19:15 -0400

    Added missing provider to Peering Deploy

--- Terraform/Networking/Deployments/Network-Deployment/Peering-Deploy/PeeringDeploy.tf
+++ Terraform/Networking/Deployments/Network-Deployment/Peering-Deploy/PeeringDeploy.tf
@@ -22,6 +22,19 @@ terraform {
   }
 }
 
+provider "azurerm" {
+  features {}
+  #(Optional) The Subscription ID which should be used. This can also be sourced from the ARM_SUBSCRIPTION_ID Environment Variable.
+  subscription_id = var.subscription_id
+  #(Optional) The Client ID which should be used. This can also be sourced from the ARM_CLIENT_ID Environment Variable.
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  #(Optional) The Tenant ID which should be used. This can also be sourced from the ARM_TENANT_ID Environment Variable.
+  tenant_id       = var.tenant_id
+  #(Optional) The Cloud Environment which should be used. Possible values are public, usgovernment, german and china. Defaults to public. This can also be sourced from the ARM_ENVIRONMENT environment variable.
+  environment = "public"
+}
+
 module "VNET-Peering" {
   source                        = "../../../VNetPeering"
   HubVNet-RGName                = data.azurerm_virtual_network.SharedServicesVNET.resource_group_name
