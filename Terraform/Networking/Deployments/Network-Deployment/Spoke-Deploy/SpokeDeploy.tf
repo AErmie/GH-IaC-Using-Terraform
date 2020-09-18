ba65e1dbd8e319d4b9b9e8de83da4fbb81486e4d
Adin Ermie <Adin.Ermie@outlook.com>
2020-09-18 12:27:54 -0400

    Update TF version, testing Workflow chain

--- Terraform/Networking/Deployments/Network-Deployment/Spoke-Deploy/SpokeDeploy.tf
+++ Terraform/Networking/Deployments/Network-Deployment/Spoke-Deploy/SpokeDeploy.tf
@@ -1,15 +1,29 @@
-# Allows what version of Terraform to use.
 terraform {
-  required_version = ">=0.12.0"
+  required_version = ">= 0.13"
+  required_providers {
+    azurerm = {
+      # The "hashicorp" namespace is the new home for the HashiCorp-maintained
+      # provider plugins.
+      #
+      # source is not required for the hashicorp/* namespace as a measure of
+      # backward compatibility for commonly-used providers, but recommended for
+      # explicitness.
+      source  = "hashicorp/azurerm"
+      version = ">= 2.0.0"
+    }
+  }
   # Backend for configuring remote state files to Azure Storage
   backend "azurerm" {
+    # NOTICE this is empty. This is because these values are supplied by the Pipeline!
+    # resource_group_name   = "TerraformStateRG"
+    # storage_account_name  = "terraformstatesaae"
+    # container_name        = "tfstate"
+    # key                   = "tf-statefile-spoke.state"
   }
 }
 
-# Configure the Azure Provider
 provider "azurerm" {
-  # While version is optional, we /strongly recommend/ using it to pin the version of the Provider being used
-  version = ">= 2.0.0"
+  features {}
   #(Optional) The Subscription ID which should be used. This can also be sourced from the ARM_SUBSCRIPTION_ID Environment Variable.
   subscription_id = var.subscription_id
   #(Optional) The Client ID which should be used. This can also be sourced from the ARM_CLIENT_ID Environment Variable.
@@ -19,7 +33,6 @@ provider "azurerm" {
   tenant_id       = var.tenant_id
   #(Optional) The Cloud Environment which should be used. Possible values are public, usgovernment, german and china. Defaults to public. This can also be sourced from the ARM_ENVIRONMENT environment variable.
   environment = "public"
-  features {}
 }
 
 module "vnets-Prod" {
